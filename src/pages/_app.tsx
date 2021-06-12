dfbb4606864e1ed61c940abfa1859df760e07962
Antonio Bernardino da Silva <tonybsilvadev@gmail.com>
2021-06-12 03:45:29 -0300

    PWA added

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,6 +10,8 @@ function App({ Component, pageProps }: AppProps) {
         <title>Ellie.On - Boilerplate</title>
         <link rel="shortcut icon" href="/img/icon-512.png" />
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
+        <link rel="manifest" href="/manifest.json" />
+
         <meta
           name="description"
           content="A simple Project starter to work with Typescript, React, NextJs and styled-components"
